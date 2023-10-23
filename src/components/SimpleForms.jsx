200a5db2118819bc3a035ab68201c9e904f96b25
Fernanda López <fer.daye.2007@gmail.com>
2023-10-23 11:07:46 -0600

    form style

--- src/components/SimpleForms.jsx
+++ src/components/SimpleForms.jsx
@@ -1,30 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './SimpeForms.css'
+import logo from '../assets/react.svg'
 
 const SimpleForms = () => {
+  const [email, setEmai] = useState('')
+  const [password, setPasword] = useState('')
+
   return (
     <>
       <div className='login'>
         <div className='login-container'>
-          <img src='{logo}' alt='logo' />
+          <img src={logo} alt='logo' />
 
           <form onSubmit={() => {}}>
             <label htmlFor='email'>Email</label>
             <input
               type='text'
-              name='emai'
+              className='formuInput'
+              name='email'
               placeholder='correogmail.com'
               id='simle-email'
-              onChange={() => {}}
-              value=''
+              onChange={(event) => { setEmai(event.target.value) }}
+              value={email}
             />
 
             <label htmlFor='password'>Pasword</label>
             <input
               type='password'
+              className='formuInput'
               name='pasword'
               id='simle-password'
-              onChange={() => {}}
-              value=''
+              onChange={(event) => { setPasword(event.target.value) }}
+              value={password}
             />
             <button type='submit'>
               iniciar Sesion
